15394ca8858ed48c3d71df380363479956dbabe5
Lidiya <simutina.1994@mail.ru>
2023-01-16 13:08:30 +0300

    Two commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balans = 211; //начальная сумма баланса
+        int balans = 100; //начальная сумма баланса
         int totalAmount; //итоговый счет
         int amount = 1100; //пополнение
-        int bonus=0;
+        //boolean amountTrue=1000;
+        int bonus = 0;
 
         if (amount > 1000) {
             bonus = amount / 100;
-            totalAmount = balans + amount;
+            totalAmount = balans + amount + bonus;
         } else {
-            totalAmount = balans + amount;
+            totalAmount = balans + amount + bonus;
         }
 
         System.out.println("Итоговый счет: " + totalAmount);
-        System.out.println("Итоговыфй Бонус: " + bonus);
+        System.out.println("Итоговый Бонус: " + bonus);
     }
 }
